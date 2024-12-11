68601696535f3d24bf01657c69c85ccdb2c8e100
Javier Nunez <javierignacio.nunez@gmail.com>
2024-12-11 18:00:51 -0300

    se realizan cambios en el Dockerfile agregando dependencias, se función notify_monitor para notificar eventos próximos, se actualizan dependencias en el requirements.txt

--- G_calendar_module/app.py
+++ G_calendar_module/app.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import os.path
-import time  # Para manejar el retraso del bucle
+import time 
+import requests
+import threading
+import logging
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.auth.transport.requests import Request
@@ -12,9 +15,14 @@ from datetime import datetime, timezone
 
 FILES_PATH = "/app/files"
 
+SERVICE_MONITOR_ENDPOINT = "http://calendar-monitor:3000/event"
+
 SERVICE_ACCOUNT_FILE = os.path.join(FILES_PATH, "serviceAccountKey.json")
 CREDENTIALS_FILE = os.path.join(FILES_PATH, "credentials.json")
 TOKEN_FILE = os.path.join(FILES_PATH, "token.json")
+# SERVICE_ACCOUNT_FILE = "serviceAccountKey.json"
+# CREDENTIALS_FILE = "credentials.json"
+# TOKEN_FILE = "token.json"
 
 cred = credentials.Certificate(SERVICE_ACCOUNT_FILE)
 firebase_admin.initialize_app(cred)
@@ -77,6 +85,25 @@ def remove_nonexistent_meetings(event_ids, year, month):
     except Exception as e:
         print(f"Error al eliminar reuniones no existentes: {e}")
 
+#Función para notificar al módulo monitor calendario
+def notify_monitor(event_id, retries=3, delay=5):
+    payload = {"eventId": event_id}
+    for attempt in range(retries):
+        try:
+            response = requests.post(SERVICE_MONITOR_ENDPOINT, json=payload)
+            response.raise_for_status()
+            logging.info(f"Notificación enviada para eventId {event_id}")
+            return
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error al notificar al servicio monitor: {e}")
+            if attempt < retries - 1:
+                time.sleep(delay)
+            else:
+                logging.error(f"No se pudo notificar al monitor despues de {retries} intentos")
+
+
+
+
 # Función principal para conectar con el calendario
 def sync_calendar():
     creds = None
@@ -114,7 +141,15 @@ def sync_calendar():
                 for entry in conference:
                     if entry.get("entryPointType") == "video":
                         start = event["start"].get("dateTime", event["start"].get("date"))
-                        event_time = dt.datetime.fromisoformat(start).strftime("%Y-%m-%d %H:%M:%S")
+                        # event_time = dt.datetime.fromisoformat(start).strftime("%Y-%m-%d %H:%M:%S")
+                
+                        start_datetime = dt.datetime.fromisoformat(start)
+                        
+
+                        start_datetime_naive = start_datetime.replace(tzinfo=None)
+                        nw = dt.datetime.now()
+                        now_naive = nw.replace(tzinfo=None)
+
 
                         attendees = event.get("attendees", [])
                         participants = [
@@ -129,6 +164,9 @@ def sync_calendar():
                         event_ids.add(event_id)
                         save_or_update_meeting(event_id, summary, start, participants, link)
 
+                        if now_naive <= start_datetime_naive <= now_naive + dt.timedelta(minutes=5):
+                            notify_monitor(event_id)
+
         remove_nonexistent_meetings(event_ids, year, month)
         print("Sincronización completada con Firestore.")
     except HttpError as error:
@@ -139,8 +177,8 @@ def main():
     while True:
         print(f"Iniciando sincronización: {datetime.now()}")
         sync_calendar()
-        print(f"Sincronización completada. Esperando 10 minutos...")
-        time.sleep(600)  # Espera de 10 minutos
+        print(f"Sincronización completada. Esperando 1 minuto...")
+        time.sleep(60)  # Espera de 1 minuto
 
 if __name__ == "__main__":
     main()
